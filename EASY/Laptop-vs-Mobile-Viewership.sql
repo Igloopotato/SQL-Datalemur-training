43fb4ba7131388a0de6761fcd355f27a1b5e97b8
mars <141108687+marswanttobeanalyst@users.noreply.github.com>
2023-09-22 11:43:34 +0800

    Update Laptop-vs-Mobile-Viewership.sql

--- EASY/Laptop-vs-Mobile-Viewership.sql
+++ EASY/Laptop-vs-Mobile-Viewership.sql
@@ -5,11 +5,9 @@ FROM Viewership;
 
 
 ----THIS IS MY PERSONAL METHOD----
--- Step 1: Add a new column to the table
 ALTER TABLE Viewership
 ADD new_device_type TEXT;
 
--- Step 2: Update the new column based on the existing column
 UPDATE Viewership
 SET new_device_type =
   CASE
@@ -18,7 +16,6 @@ SET new_device_type =
     ELSE device_type
   END;
 
--- Step 3: Select the results with the updated column and count views for 'laptop' and 'mobile' separately
 SELECT 
   SUM(CASE WHEN new_device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
   SUM(CASE WHEN new_device_type = 'mobile' THEN 1 ELSE 0 END) AS mobile_views
